e2055c79353758f73d42f362e9b74c6e2c34d35c
Zachary Pataky <zacharypataky@gmail.com>
2024-06-02 16:05:28 -0400

    DEVELOPMENT: Updated the header; About Me section; struggling to get the img to appear due to NG18 changes

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -3,11 +3,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatGridListModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
